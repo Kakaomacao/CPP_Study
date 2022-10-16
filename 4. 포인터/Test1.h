081662b5f098fe5caad495b55ff579fd4c609bbf
Inseong <tomas1997@naver.com>
2022-10-16 10:20:03 +0900

    Study 4-17

---
+++ 4. 포인터/Test1.h
@@ -0,0 +1,19 @@
+#pragma once // 여러 번 추가되더라도 한번만 선언
+
+// #ifndef  _TEST1_H__		// #prgma once랑 비슷한 기능
+// #define _TEST1_H__
+
+struct StatInfo
+{
+	int hp;
+	int attack;
+	int defense;
+};
+
+void Test_1();
+
+void Test_2();
+
+void Test_3();
+
+// #endif
