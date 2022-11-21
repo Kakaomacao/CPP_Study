ae743e990a95fe0715c37865915ad0a5d0ccef04
Inseong <tomas1997@naver.com>
2022-11-21 18:52:26 +0900

    Study 9-4

---
+++ 9. 콜백 함수/4강 탬플릿 기초.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+// 오늘의 주제 : 템플릿 기초
+
+class Knight
+{
+public:
+	// ...
+
+public:
+	int _hp = 100;
+};
+
+template<typename T> // typename 대신 class 써도 문제 없음
+void Print(T a)
+{
+	cout << a << endl;
+}
+
+// 템플릿 특수화
+// 예외적으로 특정 타입일 때는 다른 규칙으로!
+template<>
+void Print(Knight a)
+{
+	cout << "Knight !!!!!!" << endl;
+	cout << a._hp << endl;
+}
+
+
+template<typename T1, typename T2>
+void Print(T1 a, T2 b)
+{
+	cout << a << " " << b << endl;
+}
+
+template<typename T>
+T Add(T a, T b)
+{
+	return a + b;
+}
+
+// 연산자 오버로딩 (전역함수 버전)
+ostream& operator<<(ostream& os, const Knight& k)
+{
+	os << k._hp;
+	return os;
+}
+
+int main()
+{
+	// 템플릿 : 함수나 클래스를 찍어내는 틀
+	// 1) 함수 템플릿
+	// 2) 클래스 템플릿
+
+	Print<int>(50);
+	Print(50.0f);
+	Print(50.0);
+	Print("Hello World");
+
+	Print("Hello", 100);
+
+	int result1 = Add(1, 2);
+	float result2 = Add(1.11f, 2.22f);
+
+	Knight k1;
+	Print(k1);
+
+	return 0;
+}
